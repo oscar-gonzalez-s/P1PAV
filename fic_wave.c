7b03d935c72b47451e78aad2a939d39fd68885f9
poltm13 <poltm13@gmail.com>
2021-03-08 13:02:11 +0100

    Ejercicio ampliación 1

--- fic_wave.c
+++ fic_wave.c
@@ -1,30 +1,49 @@
 #include <stdio.h>
 #include "fic_wave.h"
+#include <errno.h>
 
 FILE *abre_wave(const char *ficWave, float *fm)
 {
-    FILE *fpWave;
+  FILE *fpWave;
 
-    if ((fpWave = fopen(ficWave, "r")) == NULL)
-        return NULL;
-    if (fseek(fpWave, 44, SEEK_SET) < 0)
-        return NULL;
+  if ((fpWave = fopen(ficWave, "r")) == NULL)
+    return NULL;
+  if (fseek(fpWave, 44, SEEK_SET) < 0)
+    return NULL;
 
-    int frm;
-    fseek(fpWave, 24, SEEK_SET);
-    fread(&frm, 4, 1, fpWave);
+  int nChannels;
+  fseek(fpWave, 22, SEEK_SET);
+  fread(&nChannels, 2, 1, fpWave);
+  if (nChannels > 1)
+  {
+    fprintf(stderr, "Error: Señal debe ser de un solo canal (mono)", strerror(errno));
+    return NULL;
+  }
 
-    *fm = frm;
+  int codif;
+  fseek(fpWave, 16, SEEK_SET);
+  fread(&codif, 4, 1, fpWave);
+  if (codif != 16)
+  {
+    fprintf(stderr, "Error: Señal no codificada con PCM Lineal de 16 bits", strerror(errno));
+    return NULL;
+  }
 
-    return fpWave;
+  int frm;
+  fseek(fpWave, 24, SEEK_SET);
+  fread(&frm, 4, 1, fpWave);
+
+  *fm = frm;
+
+  return fpWave;
 }
 
 size_t lee_wave(void *x, size_t size, size_t nmemb, FILE *fpWave)
 {
-    return fread(x, size, nmemb, fpWave);
+  return fread(x, size, nmemb, fpWave);
 }
 
 void cierra_wave(FILE *fpWave)
 {
-    fclose(fpWave);
+  fclose(fpWave);
 }
