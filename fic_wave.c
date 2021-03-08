dc4ae95ff8820490d8dc8111d354da1ddb736b07
poltm13 <poltm13@gmail.com>
2021-03-08 11:00:23 +0100

    commit test - remove useless code

--- fic_wave.c
+++ fic_wave.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "fic_wave.h"
 
-FILE *abre_wave(const char *ficWave, float *fm) {
+FILE *abre_wave(const char *ficWave, float *fm)
+{
     FILE *fpWave;
 
-    if ((fpWave = fopen(ficWave, "r")) == NULL) return NULL;
-    if (fseek(fpWave, 44, SEEK_SET) < 0) return NULL;
-
-    // *fm = 16000;
+    if ((fpWave = fopen(ficWave, "r")) == NULL)
+        return NULL;
+    if (fseek(fpWave, 44, SEEK_SET) < 0)
+        return NULL;
 
     int frm;
     fseek(fpWave, 24, SEEK_SET);
@@ -18,10 +19,12 @@ FILE *abre_wave(const char *ficWave, float *fm) {
     return fpWave;
 }
 
-size_t lee_wave(void *x, size_t size, size_t nmemb, FILE *fpWave) {
+size_t lee_wave(void *x, size_t size, size_t nmemb, FILE *fpWave)
+{
     return fread(x, size, nmemb, fpWave);
 }
 
-void cierra_wave(FILE *fpWave) {
+void cierra_wave(FILE *fpWave)
+{
     fclose(fpWave);
 }
